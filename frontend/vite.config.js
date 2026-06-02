2a88fbfa736dae88c55d7e2518dc1936b3cfc805
Rashi Singh <rashi1912singh@gmail.com>
2026-06-02 19:05:20 +0530

    Allow Render host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT,
+    allowedHosts: ['safespace-3vz1.onrender.com']
+  }
 })
